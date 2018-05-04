cd84d24a899c06b4545559ce5299073bbc7ced20
mwallewein <wallewein@creatdev-solutions.com>
2018-05-04 14:21:08 +0200

    Add first test for priority queue

--- testing/test_runner.cpp
+++ testing/test_runner.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include "tests/threadpool/test_pool.hpp"
+#include "tests/priorityqueue/test_queue.hpp"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
