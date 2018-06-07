08645f71a6b10ab0ba600b6dd540a698e83b719b
martenwallewein <wallewein@creatdev-solutions.com>
2018-06-07 14:03:05 +0200

    Go back to gtest

--- testing/test_runner.c
+++ testing/test_runner.c
@@ -1,36 +1,10 @@
-#include <check.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "tests/thread_pool/test_thread_pool.h"
-
-Suite * thread_pool_suite(void)
-{
-    Suite *s;
-    TCase *tc_core;
-
-    s = suite_create("Thread Pool");
-
-    /* Core test case */
-    tc_core = tcase_create("Core");
-
-    tcase_add_test(tc_core, thread_pool_create_);
-    tcase_add_test(tc_core, thread_pool_name);
-    //tcase_add_test(tc_core, thread_pool_tasks);
-    tcase_add_test(tc_core, thread_pool_tasks_wait);
-    
-
-    suite_add_tcase(s, tc_core);
-
-    return s;
-}
-
 int main(void)
 {
     int number_failed;
     Suite *s;
     SRunner *sr;
 
-    s = thread_pool_suite();
+    s = money_suite();
     sr = srunner_create(s);
 
     srunner_run_all(sr, CK_NORMAL);
